ea952f73c315aaf4619d983dea752eb4b266e2f2
sergiu221@gmail.com <sergiu221@gmail.com>
2020-05-16 17:13:44 +0200

    Register this service to Spring Cloud

--- src/main/java/com/exam/api/gateway/security/WebSecurity.java
+++ src/main/java/com/exam/api/gateway/security/WebSecurity.java
@@ -24,10 +24,12 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 	public void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 		http.headers().frameOptions().disable();
-		http.authorizeRequests().antMatchers(HttpMethod.POST, environment.getProperty("api.registration.url.path"))
-				.permitAll().antMatchers(HttpMethod.POST, environment.getProperty("api.login.url.path")).permitAll()
-				.anyRequest().authenticated().and()
-				.addFilter(new AuthorizationFilter(authenticationManager(), environment));
+		http.authorizeRequests()
+		.antMatchers(HttpMethod.POST, environment.getProperty("api.registration.url.path")).permitAll()
+		.antMatchers(HttpMethod.POST, environment.getProperty("api.login.url.path")).permitAll()
+		.anyRequest().authenticated()
+		.and()
+		.addFilter(new AuthorizationFilter(authenticationManager(), environment));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 }
